735fb904056e7e3dc4002f04a15d279bd9263e29
minhnd90 <258006_evn@etgrp.onmicrosoft.com>
2023-10-06 17:28:40 +0700

    Config vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: ['assets/styles/main.scss'],
+  modules: [
+    // https://nuxt.com/modules/vuetify-nuxt-module
+    'vuetify-nuxt-module',
+    // https://google-fonts.nuxtjs.org/
+    '@nuxtjs/google-fonts',
+  ],
+  vuetify: {
+    vuetifyOptions: {
+      display: {
+        mobileBreakpoint: 'md',
+        thresholds: {
+          xs: 0,
+          sm: 340,
+          md: 540,
+          lg: 768,
+          xl: 1280,
+          xxl: 1440,
+        },
+      },
+      labComponents: true,
+    },
+    moduleOptions: {
+      styles: {
+        configFile: 'assets/styles/settings.scss',
+      },
+    },
+  },
+  experimental: {
+    inlineSSRStyles: process.env.NODE_ENV !== 'production',
+  },
+});
